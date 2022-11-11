f937dd9f12e10ecdeea690b84559bbd84beb1866
shermzlim <shermz.lim.jh@gmail.com>
2022-11-11 07:40:14 +0000

    complete mesi protocol

--- cache_controller_mesi.cpp
+++ cache_controller_mesi.cpp
@@ -51,21 +51,51 @@ void CacheControllerMesi::handle_bus_resp(BusTransaction transc) {
   CacheBlock block_no = transc.block;
   State state = get_state(block_no);
   if (state == State::INVALID && transc.t == BusTransactionType::BUS_RD) {
-    cache.insert_block(block_no);
-    cache.read_block(block_no);
     State new_state = shared_line.assert_line(block_no) ? State::SHARED : State::EXCLUSIVE;
     update_state(block_no, new_state);
+    cache.read_block(block_no);
   } else if (state == State::INVALID && transc.t == BusTransactionType::BUS_RDX) {
-    cache.insert_block(block_no);
-    cache.write_block(block_no);
     update_state(block_no, State::MODIFIED);
+    cache.write_block(block_no);
   } else {
     assert(false);
   }
 }
 
 bool CacheControllerMesi::handle_bus_transc(BusTransaction transc) {
-  
+  CacheBlock block_no = transc.block;
+  BusTransactionType t = transc.t;
+  switch (get_state(block_no)) {
+    case State::INVALID:
+      return false;
+    case State::EXCLUSIVE:
+      // no cache-to-cache sharing
+      if (t == BusTransactionType::BUS_RD) {
+        update_state(block_no, State::SHARED);
+        return false;
+      } else if (t == BusTransactionType::BUS_RDX) {
+        update_state(block_no, State::INVALID);
+        return false;
+      }
+    case State::SHARED:
+      // no cache-to-cache sharing
+      if (t == BusTransactionType::BUS_RD) {
+        return false;
+      } else if (t == BusTransactionType::BUS_RDX || t == BusTransactionType::BUS_UPGR) {
+        update_state(block_no, State::INVALID);
+        return false;
+      }
+    case State::MODIFIED:
+      if (t == BusTransactionType::BUS_RD) {
+        update_state(block_no, State::SHARED);
+        return true;
+      } else if (t == BusTransactionType::BUS_RDX) {
+        update_state(block_no, State::INVALID);
+        return true;
+      }
+  }
+  assert(false);
+  return false;
 }
 
 CacheControllerMesi::State CacheControllerMesi::get_state(CacheBlock block_no) {
@@ -77,5 +107,15 @@ CacheControllerMesi::State CacheControllerMesi::get_state(CacheBlock block_no) {
 }
 
 void CacheControllerMesi::update_state(CacheBlock block_no, State state) {
+  State old_state = get_state(block_no);
+  assert(old_state != state);
+  // update block's state
   blocks_state[block_no] = state;
+  // update cache
+  if (old_state == State::INVALID) { // Invalid -> Shared/Exclusive/Modified
+    cache.insert_block(block_no);
+  } else if (state == State::INVALID) { // Shared/Exclusive/Modified -> Invalid
+    bool dirty = cache.remove_block(block_no);
+    assert(old_state != State::MODIFIED || dirty);
+  }
 }

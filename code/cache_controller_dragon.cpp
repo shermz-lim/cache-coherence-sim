e36271a007c6a371b975a273ced629da61ae7315
shermzlim <shermz.lim.jh@gmail.com>
2022-11-16 20:42:01 +0800

    add basic dragon methods

--- code/cache_controller_dragon.cpp
+++ code/cache_controller_dragon.cpp
@@ -24,15 +24,38 @@ bool CacheControllerDragon::handle_bus_transc(BusTransaction transc) {
 }
 
 void CacheControllerDragon::evict_block(CacheBlock block_no) {
+  blocks_state.erase(block_no);
 }
 
 void CacheControllerDragon::print_state() {
+  std::cout << "------- DragonController " << core_no << " state -------";
+  for (const auto& it : blocks_state) {
+    std::cout << std::endl << it.first << ":" << state_to_string(it.second);
+  }
+  std::cout << std::endl;
 }
 
 std::string_view CacheControllerDragon::state_to_string(State s) {
+  switch (s) {
+    case State::INVALID:
+      return "INVALID";
+    case State::EXCLUSIVE:
+      return "EXCLUSIVE";
+    case State::SHARED_CLEAN:
+      return "SHARED_CLEAN";
+    case State::SHARED_MODIFIED:
+      return "SHARED_MODIFIED";
+    case State::MODIFIED:
+      return "MODIFIED";
+  }
 }
 
 CacheControllerDragon::State CacheControllerDragon::get_state(CacheBlock block_no) {
+  if (blocks_state.count(block_no) == 0) {
+    return State::INVALID;
+  } else {
+    return blocks_state.at(block_no);
+  }
 }
 
 void CacheControllerDragon::update_state(CacheBlock block_no, State state) {
